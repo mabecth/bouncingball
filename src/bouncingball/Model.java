90954f704bbb7d0e9fd397d15e39a0dfe5a533d5
Marcus <mabe.cth@gmail.com>
2017-04-11 01:40:53 +0200

    Further work
    Finished convertion methods and added basic bouncing

--- src/bouncingball/Model.java
+++ src/bouncingball/Model.java
@@ -2,8 +2,6 @@ package bouncingball;
 
 import com.sun.javafx.geom.Vec2d;
 
-import java.util.Vector;
-
 /**
  * The physics model.
  *
@@ -19,6 +17,7 @@ class Model {
     double areaWidth, areaHeight;
 
     Ball [] balls;
+    double time = 0;
 
     Model(double width, double height) {
         areaWidth = width;
@@ -27,7 +26,7 @@ class Model {
         // Initialize the model with a few balls
         balls = new Ball[2];
         balls[0] = new Ball(width / 3, height * 0.9, 1.2, 1.6, 0.2,0.2);
-        balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3,0.2);
+        balls[1] = new Ball(2 * width / 3, height * 0.7, -0.6, 0.6, 0.3,0.5);
     }
 
     void step(double deltaT) {
@@ -41,24 +40,33 @@ class Model {
                 b.vy *= -1;
             }
 
-            // compute new position according to the speed of the ball
+            // compute new position during free fall according to euler's method
+            time += deltaT;
+
+            //x'' = 0
             b.x += deltaT * b.vx;
-            b.y += deltaT * b.vy;
+
+            //F = my''
+            b.y += deltaT * (b.vy + deltaT * b.g / 2);
+            b.vy += deltaT * b.g;
         }
     }
 
     //http://www.teacherschoice.com.au/maths_library/coordinates/polar_-_rectangular_conversion.htm
+    //Convert rectangular coordinates to polar
     Vec2d rectToPolar(double x, double y) {
         double r = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
         double q = Math.atan(y/x);
 
-
         return new Vec2d(r,q);
-
     }
 
-    Vec2d polarToRect() {
-        return new Vec2d();
+    //Convert polar coordinates to rectangular
+    Vec2d polarToRect(double r, double q) {
+        double x =r*Math.cos(q);
+        double y =r*Math.sin(q);
+
+        return new Vec2d(x,y);
     }
 
     /**
@@ -73,12 +81,16 @@ class Model {
             this.vy = vy;
             this.radius = r;
             this.m = m;
+            kineticX = (m * Math.pow(vx,2)) / 2;
+            kineticY = (m * Math.pow(vy,2)) / 2;
+            momentumX = m * vx;
+            momentumY = m * vy;
         }
 
         /**
          * Position, speed, and radius of the ball. You may wish to add other attributes.
          */
-        double x, y, vx, vy, radius, m;
+        double x, y, vx, vy, radius, m, kineticX, kineticY, momentumX, momentumY;
         double g = -9.82;
     }
 }
