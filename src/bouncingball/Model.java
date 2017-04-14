1ebb67f015efa1c3d53d258e970f3d167c032b49
Marcus Bertilsson <mabe.cth@gmail.com>
2017-04-14 17:02:28 +0200

    Improved collisions
    Still need some work'

--- src/bouncingball/Model.java
+++ src/bouncingball/Model.java
@@ -18,6 +18,7 @@ class Model {
 
     Ball [] balls;
     double time = 0;
+    boolean collisionHandled = false;
 
     Model(double width, double height) {
         areaWidth = width;
@@ -25,8 +26,8 @@ class Model {
 
         // Initialize the model with a few balls
         balls = new Ball[2];
-        balls[0] = new Ball(width / 3, height * 0.5, 0.8, 0, 0.2,0.2);
-        balls[1] = new Ball(3 * width / 3, height * 0.9, -0.4, 0, 0.3,0.5);
+        balls[0] = new Ball(width / 3, height * 0.6, 4, 0, 0.2,3);
+        balls[1] = new Ball(2 * width / 3, height * 0.6, -6, 0, 0.3,5);
     }
 
     void step(double deltaT) {
@@ -38,10 +39,11 @@ class Model {
 
                 if(!b.equals(b2)) {
                     if (checkCollision(b, b2)) {
-                        solveOverlap(b,b2);
-                        handleCollision(b, b2);
+                        handleCollision(b, b2, b.vx, b2.vx);
                         move(b, deltaT);
                         move(b2, deltaT);
+                        //System.out.println(rectToPolar(-1,-1));
+                        //System.exit(0);
                     }
                 }
             }
@@ -120,53 +122,60 @@ class Model {
      * @param b1 the first ball
      * @param b2 the second ball
      */
-    void handleCollision(Ball b1, Ball b2) {
+    void handleCollision(Ball b1, Ball b2, double u1, double u2) {
 
-        Vec2d v1;
-        Vec2d v2;
+        if (!collisionHandled) {
+            Vec2d v1;
+            Vec2d v2;
 
-        //Calculate dx and dy
-        double dx = Math.abs(b1.x - b2.x);
-        double dy = Math.abs(b1.y - b2.y);
+            //Calculate dx and dy
+            double dx = Math.abs(b1.x - b2.x);
+            double dy = Math.abs(b1.y - b2.y);
 
-        //Calculate angle between b1 and b2
-        double beta = rectToPolar(dx, dy).y;
+            //Calculate angle between b1 and b2
+            double beta = rectToPolar(dx, dy).y;
 
-        //Convert to polar coordinates
-        v1 = rectToPolar(b1.vx, b1.vy);
-        v2 = rectToPolar(b2.vx, b2.vy);
+            //Convert to polar coordinates
+            v1 = rectToPolar(b1.vx, b1.vy);
+            v2 = rectToPolar(b2.vx, b2.vy);
 
-        //Calculate angle in new coordinate system
-        v1.y -= beta;
-        v2.y -= beta;
+            //Calculate angle in new coordinate system
+            v1.y -= beta;
+            v2.y -= beta;
 
-        //Convert to rectangular coordinates
-        v1 = polarToRect(v1.x, v1.y);
-        v2 = polarToRect(v2.x, v2.y);
+            //Convert to rectangular coordinates
+            v1 = polarToRect(v1.x, v1.y);
+            v2 = polarToRect(v2.x, v2.y);
 
-        //Velocities before collision
-        double u1 = v1.x;
-        double u2 = v2.x;
+            //Calculate new velocities (elastic collision)
+            v1.x = (u1 * (b1.m - b2.m) + 2 * b2.m * u2) / (b1.m + b2.m);
+            v2.x = (u2 * (b2.m - b1.m) + 2 * b1.m * u1) / (b1.m + b2.m);
 
-        //Calculate new velocities (elastic collision)
-        v1.x = (u1 * (b1.m - b2.m) + 2 * b2.m * u2) / (b1.m + b2.m);
-        v2.x = (u2 * (b2.m - b1.m) + 2 * b1.m * u1) / (b1.m + b2.m);
+            //System.out.println(v1.x + " " + v2.x + " " + v1.y + " " + v2.y);
+            //Convert to polar coordinates
+            v1 = rectToPolar(v1.x, v1.y);
+            v2 = rectToPolar(v2.x, v2.y);
 
-        //Convert to polar coordinates
-        v1 = rectToPolar(v1.x, v1.y);
-        v2 = rectToPolar(v1.x, v2.y);
+            //Set back to old angle
+            v1.y += beta;
+            v2.y += beta;
 
-        //Set back to old angle
-        v1.y += beta;
-        v2.y += beta;
+            //Convert back to x/y coordinates
+            v1 = polarToRect(v1.x, v1.y);
+            v2 = polarToRect(v2.x, v2.y);
+            //System.out.println(v1.x + " " + v2.x + " " + v1.y + " " + v2.y);
+            b1.vx = v1.x;
+            b1.vy = v1.y;
+            b2.vx = v2.x;
+            b2.vy = v2.y;
 
-        //Convert back to x/y coordinates
-        v1 = polarToRect(v1.x, v1.y);
-        v2 = polarToRect(v2.x, v2.y);
-        b1.vx = v1.x;
-        b1.vy = v1.y;
-        b2.vx = v2.x;
-        b2.vy = v2.y;
+            System.out.println("b1 " + "vx: " + b1.vx + " r: " + b1.radius + "\nb2 " + "vx: " + b2.vx + " r: " + b2.radius);
+
+            solveOverlap(b1, b2);
+            collisionHandled = true;
+        } else {
+            collisionHandled = false;
+        }
     }
 
     /**
@@ -179,31 +188,30 @@ class Model {
         double dx = (b1.x - b2.x);
         double dy = (b1.y - b2.y);
         double radiusSum = b1.radius + b2.radius;
-
-        if (dx <=  0) {
-            double diffX = Math.abs(dx - radiusSum);
+        if (dx <  0) {
+            double diffX = dx + radiusSum;
             if (diffX > 0) {
                 b1.x -= diffX / 2;
                 b2.x += diffX / 2;
             }
-        } else {
-            double diffX = Math.abs(dx - radiusSum);
+        } else if (dx > 0) {
+            double diffX = dx - radiusSum;
             if (diffX < 0) {
                 b1.x -= diffX / 2;
                 b2.x += diffX / 2;
             }
         }
-        if (dy <= 0) {
-            double diffY = Math.abs(dy - radiusSum);
+        if (dy < 0) {
+            double diffY = dy + radiusSum;
             if (diffY > 0) {
-                b1.x -= diffY / 2;
-                b2.x += diffY / 2;
+                b1.y -= diffY / 2;
+                b2.y += diffY / 2;
             }
-        } else {
-            double diffY = Math.abs(dy - radiusSum);
+        } else if (dy > 0) {
+            double diffY = dy - radiusSum;
             if (diffY < 0) {
-                b1.x -= diffY / 2;
-                b2.x += diffY / 2;
+                b1.y -= diffY / 2;
+                b2.y += diffY / 2;
             }
         }
     }
@@ -215,7 +223,30 @@ class Model {
      */
     Vec2d rectToPolar(double x, double y) {
         double r = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
-        double q = Math.atan(y/x);
+        double q = 0;
+        if (x >= 0 && y >= 0) {
+            //First quadrant
+            q = Math.atan(y/x);
+            System.out.println("First");
+        } else if(x < 0 && y > 0) {
+            //Second quadrant
+            if (Math.atan(y/x) != 0)
+                r *= -1;
+            q = Math.atan(y/x);// + Math.PI;
+            System.out.println("Second");
+        } else if(x < 0 && y < 0) {
+            //Third quadrant
+            if (Math.atan(y/x) != 0)
+                r *= -1;
+            q = Math.atan(y/x); //+ Math.PI;
+            System.out.println("Third");
+        } else {
+            //Fourth quadrant
+            if (Math.atan(y/x) != 0)
+            q = Math.atan(y/x);// Math.PI * 2;
+            r *= -1;
+            System.out.println("Fourth");
+        }
 
         return new Vec2d(r, q);
     }
